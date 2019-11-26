10d8498237e690dcaa4633c4a387e76673e0b38e
Ivana Livaja <ivana.livaja@hotmail.com>
2019-11-26 22:38:53 +0100

    update

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default class Recipe extends Component {
 	render() {
-		const { image, label, calories } = this.props.recipe;
+		const { image, label, calories, healthLabels } = this.props.recipe;
 		const ingredients = this.props.recipe.ingredients;
 
 		return (
@@ -19,12 +19,23 @@ export default class Recipe extends Component {
 							}
 						}}
 					>
-						<div className="card cards">
+						<div className="card h-100 cards">
 							<img src={image} className="img-card-top image" style={{ height: 'auto' }} alt="recipe" />
-							<div className="card-body text-capitalize">
-								<h2>{label}</h2>
-								<h6 className="d-inline p-1 yield">Number of Servings: {this.props.recipe.yield} </h6>
-								<h6 className="d-inline p-1 cal">Calories: {Math.round(calories)} </h6>
+							<div className="card-body">
+								<h2 title={label} className="mb-0">
+									{label.length < 20 ? `${label}` : `${label.substring(0, 18)}...`}
+								</h2>
+								<div className="card-body">
+									{healthLabels.map((e, i) => {
+										return (
+											<p key={i} className="d-inline p-1 health-labels">
+												{e}
+											</p>
+										);
+									})}
+								</div>
+								<h6 className="d-inline p-2 yield">Number of Servings: {this.props.recipe.yield} </h6>
+								<h6 className="d-inline p-2 cal">Calories: {Math.round(calories)} </h6>
 							</div>
 						</div>
 					</Link>
